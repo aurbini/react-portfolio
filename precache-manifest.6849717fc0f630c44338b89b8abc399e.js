f23d4dd8de53f94000874ed5722e734f5fc7c6ca
Alex Urbini <aurbini92@gmail.com>
2020-03-10 10:05:30 -0400

    Updates

---
+++ precache-manifest.6849717fc0f630c44338b89b8abc399e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7d2d9f1dbf6e72c17e96f95e3fc02f8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b4b1746d3cfc0308a224",
+    "url": "/react-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bfe6988545d72c4eb8a7",
+    "url": "/react-portfolio/static/css/main.b55ccfb5.chunk.css"
+  },
+  {
+    "revision": "b4b1746d3cfc0308a224",
+    "url": "/react-portfolio/static/js/2.81b238a7.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/react-portfolio/static/js/2.81b238a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe6988545d72c4eb8a7",
+    "url": "/react-portfolio/static/js/main.7723fc14.chunk.js"
+  },
+  {
+    "revision": "6c1a97d7eedcd4b749e8",
+    "url": "/react-portfolio/static/js/runtime-main.fcfd7c4a.js"
+  },
+  {
+    "revision": "00761b62898aa44be7289a57b899bd78",
+    "url": "/react-portfolio/static/media/Password.00761b62.png"
+  },
+  {
+    "revision": "d90a238d3ae8df289edc4b07d717ce37",
+    "url": "/react-portfolio/static/media/Recycle.d90a238d.png"
+  },
+  {
+    "revision": "61cfea5d2803257611a59396adeb701c",
+    "url": "/react-portfolio/static/media/TeaCozy.61cfea5d.png"
+  },
+  {
+    "revision": "84cd1e1e8aef47b0fccf1f35b6224ece",
+    "url": "/react-portfolio/static/media/WeatherDash.84cd1e1e.png"
+  },
+  {
+    "revision": "85d8a263b560a71113010f5a23ecf54e",
+    "url": "/react-portfolio/static/media/budget.85d8a263.png"
+  },
+  {
+    "revision": "1e7d191123e4884dab027cc7637c391a",
+    "url": "/react-portfolio/static/media/selfie.1e7d1911.jpg"
+  },
+  {
+    "revision": "a38780a11a18e06b5348a0858df2b343",
+    "url": "/react-portfolio/static/media/spiderman.a38780a1.jpg"
+  },
+  {
+    "revision": "8cfe8cf25e867c4f412851bbfd9fd291",
+    "url": "/react-portfolio/static/media/whetherFun.8cfe8cf2.png"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/react-portfolio/static/media/white-waves.a06a3248.png"
+  }
+]);
